ac9f43ed44ded7fb8ce46d03a949150e8dbec20f
Yasas <ezioelijahhanaang@gmail.com>
2025-11-07 15:14:07 +0530

    hugging face intergration for trained transformer model

--- src/detectors/vit_detector.py
+++ src/detectors/vit_detector.py
@@ -12,23 +12,11 @@ PROJECT_ROOT = os.path.dirname(BASE_DIR)
 LOCAL_MODEL_DIR = os.path.join(PROJECT_ROOT, "vit-local")  
 
 class ViTEmotionModel:
-    def __init__(self, model_path, num_classes=7, device=None):
+    def __init__(self, repo_id="yst007/vit-emotion", device=None):
         self.device = device or ("cuda" if torch.cuda.is_available() else "cpu")
-
-        
-        config = ViTConfig.from_pretrained("google/vit-base-patch16-224-in21k")
-        config.num_labels = num_classes
-
-    
-        self.model = ViTForImageClassification(config)
-        self.model.load_state_dict(torch.load(model_path, map_location=self.device))
-        self.model.to(self.device)
+        self.model = ViTForImageClassification.from_pretrained(repo_id).to(self.device)
+        self.extractor = ViTImageProcessor.from_pretrained(repo_id)
         self.model.eval()
-
-        
-        self.extractor = ViTImageProcessor.from_pretrained(LOCAL_MODEL_DIR)
-        self.img_size = self.extractor.size['height']
-        self.num_classes = num_classes
 
     def predict(self, face_bgr: np.ndarray):
         face_rgb = cv2.cvtColor(face_bgr, cv2.COLOR_BGR2RGB)
